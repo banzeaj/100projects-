b0de08cb1929540a163993eb6ba92f59a8c55ad3
banzeaj <banze.aj@gmail.com>
2024-07-21 23:17:46 +0200

    random-Photo

---
+++ Random-Photes/index.js
@@ -0,0 +1,15 @@
+
+const imageContainerE1 = document.querySelector(".image-container");
+
+const btnE1 = document.querySelector(".btn");
+
+btnE1.addEventListener("click",()=>{
+
+
+})
+
+function addNewImages(){
+     
+    const newImgE1 = document.createElement("img")
+    newImgE1.src`https://picsum.photos/300?random=$`
+}
