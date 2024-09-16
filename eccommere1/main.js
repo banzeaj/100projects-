490eff44ae7c953a35a8bf58989459005497a709
banzeaj <banze.aj@gmail.com>
2024-09-16 21:47:53 +0200

    second

--- eccommere1/main.js
+++ eccommere1/main.js
@@ -54,10 +54,47 @@ function addEvents(){
     cartQuantity_inputs.forEach(input =>{
         input.addEventListener("change",handle_changeItemQuantity);
     })
+
+    //add item to cart 
+
+    let addCart_btns =document.querySelectorAll(".add-cart");
+    addCart_btns.forEach((btn)=>{
+        btn.addEventListener("click",handle_addCartItem);
+    
+    });
+
 }
 
 //==============handle==================
+function handle_addCartItem(){
 
+    let product = this.parentElement;
+    let title = product.querySelector(".product-title").innerHTML;
+    let price = product.querySelector(".product-price").innerHTML;
+    let imgSrc =product.querySelector(".product-img").src;
+
+    console.log(title, price, imgSrc)
+
+    let newToAdd = {
+        title,
+        price,
+        imgSrc,
+    };
+    //handle item is already exist 
+    // start here
+
+    //  add product to cart 
+
+    let cartBoxElement = CartBoxComponent(title,price,imgSrc);
+    let newNode = document.createElement("div");
+    newNode.innerHTML = cartBoxElement;
+    const cartContent = cart.querySelector(".cart-content");  
+    cartContent.appendChild(newNode); 
+    update();  
+
+     
+    
+}
 function handle_removeCartItem(){   
 
     this.parentElement.remove();
@@ -82,8 +119,7 @@ function updateTotal() {
 
     let cartBoxes = document.querySelectorAll(".cart-box");
     const totalElement = cart.querySelector(".total-price");
-    let total = 0;
-
+    let total = 0;  
     cartBoxes.forEach((cartBox) => {
 
         let priceElement = cartBox.querySelector(".cart-price");
@@ -91,9 +127,27 @@ function updateTotal() {
         let quantity = cartBox.querySelector(".cart-quantity").value;
         total += price * quantity;
     });
+
+    // keep 2 digits after the decimal point
+    total =total.toFixed(2);
     totalElement.innerHTML = "R" + total; 
 
+} 
+
+//======== update & reference function ===========
+function CartBoxComponent(title,price,imgSrc){
+
+
+             return`
+                        <div class="cart-box">
+                        <img src=${imgSrc}alt=""class="cart-img">
+                        <div clas="detail-box">
+                            <div class="cart-product-title">${title} </div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                        </div>
+
+                        <!--remove cart-->
+                        <i class ="bx bxs-trash-alt cart-remove"></i>
+                    </div>`;
 }
-
-
-//========================================================================
