6d1590f1732f34e6e7c0fe4ed51ada44c8e167f4
banzeaj <banze.aj@gmail.com>
2024-09-18 12:15:01 +0200

    done

--- eccommere1/main.js
+++ eccommere1/main.js
@@ -1,153 +1,158 @@
-
-// open and close cart 
-
+// OPEN & CLOSE CART
 const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
-const closeCart =  document.querySelector("#cart-close");
+const closeCart = document.querySelector("#cart-close");
 
-cartIcon.addEventListener("click",()=>{
-    cart.classList.add("active");
+cartIcon.addEventListener("click", () => {
+  cart.classList.add("active");
 });
 
-closeCart.addEventListener("click",()=>{
-    cart.classList.remove("active");
+closeCart.addEventListener("click", () => {
+  cart.classList.remove("active");
 });
 
-alert("check the code again ")
-
-// start when the document is ready 
-
-if(document.readyState=="loading"){
-    document.addEventListener('DOMContentLoaded',start);
-
-}else{
-    start();
+// Start when the document is ready
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", start);
+} else {
+  start();
 }
 
-//========================start============= 
-function start(){
-addEvents();
-
+// =============== START ====================
+function start() {
+  addEvents();
 }
 
-//=================update====================
-function update(){
-    addEvents();
-    updateTotal();
-
+// ============= UPDATE & RERENDER ===========
+function update() {
+  addEvents();
+  updateTotal();
 }
 
-//===================addd events==============
+// =============== ADD EVENTS ===============
+function addEvents() {
+  // Remove items from cart
+  let cartRemove_btns = document.querySelectorAll(".cart-remove");
+  console.log(cartRemove_btns);
+  cartRemove_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_removeCartItem);
+  });
 
+  // Change item quantity
+  let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
+  cartQuantity_inputs.forEach((input) => {
+    input.addEventListener("change", handle_changeItemQuantity);
+  });
 
-function addEvents(){ 
-    //remove items from cart 
-    let cartRemove_btns = document.querySelectorAll(".cart-remove");
-    console.log(cartRemove_btns);
-    cartRemove_btns.forEach ((btn)=>(
-    btn.addEventListener("click",handle_removeCartItem)
-    ));
-  
-    // changge items quantity
+  // Add item to cart
+  let addCart_btns = document.querySelectorAll(".add-cart");
+  addCart_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_addCartItem);
+  });
 
-    let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
-    cartQuantity_inputs.forEach(input =>{
-        input.addEventListener("change",handle_changeItemQuantity);
-    })
-
-    //add item to cart 
-
-    let addCart_btns =document.querySelectorAll(".add-cart");
-    addCart_btns.forEach((btn)=>{
-        btn.addEventListener("click",handle_addCartItem);
-    
-    });
-
+  // Buy Order
+  const buy_btn = document.querySelector(".btn-buy");
+  buy_btn.addEventListener("click", handle_buyOrder);
 }
 
-//==============handle==================
-function handle_addCartItem(){
+// ============= HANDLE EVENTS FUNCTIONS =============
+let itemsAdded = [];
 
-    let product = this.parentElement;
-    let title = product.querySelector(".product-title").innerHTML;
-    let price = product.querySelector(".product-price").innerHTML;
-    let imgSrc =product.querySelector(".product-img").src;
+function handle_addCartItem() {
+  let product = this.parentElement;
+  let title = product.querySelector(".product-title").innerHTML;
+  let price = product.querySelector(".product-price").innerHTML;
+  let imgSrc = product.querySelector(".product-img").src;
+  console.log(title, price, imgSrc);
 
-    console.log(title, price, imgSrc)
+  let newToAdd = {
+    title,
+    price,
+    imgSrc,
+  };
 
-    let newToAdd = {
-        title,
-        price,
-        imgSrc,
-    };
-    //handle item is already exist 
-    // start here
+  // handle item is already exist
+  if (itemsAdded.find((el) => el.title == newToAdd.title)) {
+    alert("This Item Is Already Exist!");
+    return;
+  } else {
+    itemsAdded.push(newToAdd);
+  }
 
-    //  add product to cart 
+  // Add product to cart
+  let cartBoxElement = CartBoxComponent(title, price, imgSrc);
+  let newNode = document.createElement("div");
+  newNode.innerHTML = cartBoxElement;
+  const cartContent = cart.querySelector(".cart-content");
+  cartContent.appendChild(newNode);
 
-    let cartBoxElement = CartBoxComponent(title,price,imgSrc);
-    let newNode = document.createElement("div");
-    newNode.innerHTML = cartBoxElement;
-    const cartContent = cart.querySelector(".cart-content");  
-    cartContent.appendChild(newNode); 
-    update();  
-
-     
-    
-}
-function handle_removeCartItem(){   
-
-    this.parentElement.remove();
-
-    update();
+  update();
 }
 
-function handle_changeItemQuantity(){
+function handle_removeCartItem() {
+  this.parentElement.remove();
+  itemsAdded = itemsAdded.filter(
+    (el) =>
+      el.title !=
+      this.parentElement.querySelector(".cart-product-title").innerHTML
+  );
 
-    if(isNaN(this.value)|| this.value<1){ 
-        this.value =1;
-
-    }
-    this.value =math.floor(this.value); // to keep it integer
-    update();
+  update();
 }
 
+function handle_changeItemQuantity() {
+  if (isNaN(this.value) || this.value < 1) {
+    this.value = 1;
+  }
+  this.value = Math.floor(this.value); // to keep it integer
 
-// =============update $ reference function=======
+  update();
+}
 
+function handle_buyOrder() {
+  if (itemsAdded.length <= 0) {
+    alert("There is No Order to Place Yet! \nPlease Make an Order first.");
+    return;
+  }
+  const cartContent = cart.querySelector(".cart-content");
+  cartContent.innerHTML = "";
+  alert("Your Order is Placed Successfully :)");
+  itemsAdded = [];
+
+  update();
+}
+
+// =========== UPDATE & RERENDER FUNCTIONS =========
 function updateTotal() {
+  let cartBoxes = document.querySelectorAll(".cart-box");
+  const totalElement = cart.querySelector(".total-price");
+  let total = 0;
+  cartBoxes.forEach((cartBox) => {
+    let priceElement = cartBox.querySelector(".cart-price");
+    let price = parseFloat(priceElement.innerHTML.replace("$", ""));
+    let quantity = cartBox.querySelector(".cart-quantity").value;
+    total += price * quantity;
+  });
 
-    let cartBoxes = document.querySelectorAll(".cart-box");
-    const totalElement = cart.querySelector(".total-price");
-    let total = 0;  
-    cartBoxes.forEach((cartBox) => {
+  // keep 2 digits after the decimal point
+  total = total.toFixed(2);
+  // or you can use also
+  // total = Math.round(total * 100) / 100;
 
-        let priceElement = cartBox.querySelector(".cart-price");
-        let price = parseFloat(priceElement.innerHTML.replace("R",""));
-        let quantity = cartBox.querySelector(".cart-quantity").value;
-        total += price * quantity;
-    });
+  totalElement.innerHTML = "$" + total;
+}
 
-    // keep 2 digits after the decimal point
-    total =total.toFixed(2);
-    totalElement.innerHTML = "R" + total; 
-
-} 
-
-//======== update & reference function ===========
-function CartBoxComponent(title,price,imgSrc){
-
-
-             return`
-                        <div class="cart-box">
-                        <img src=${imgSrc}alt=""class="cart-img">
-                        <div clas="detail-box">
-                            <div class="cart-product-title">${title} </div>
-                            <div class="cart-price">${price}</div>
-                            <input type="number" value="1" class="cart-quantity">
-                        </div>
-
-                        <!--remove cart-->
-                        <i class ="bx bxs-trash-alt cart-remove"></i>
-                    </div>`;
+// ============= HTML COMPONENTS =============
+function CartBoxComponent(title, price, imgSrc) {
+  return `
+    <div class="cart-box">
+        <img src=${imgSrc} alt="" class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
+            <input type="number" value="1" class="cart-quantity">
+        </div>
+        <!-- REMOVE CART  -->
+        <i class='bx bxs-trash-alt cart-remove'></i>
+    </div>`;
 }
